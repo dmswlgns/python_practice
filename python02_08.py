fd64d1603a5fe8da2520ff8076c9f0335be93ae6
jihun <altmxjdms@naver.com>
2024-02-08 10:21:26 +0900

    02_08_v1.1

--- python02_08.py
+++ python02_08.py
@@ -1,1 +1,23 @@
-print("hello python")
+
+def factorial(num) -> int :
+    pass
+
+def nCr(n,r) -> int :
+    '''
+    조합 함수
+    :param n: 총 개수
+    :param r: 뽑을 개수
+    :return: 경우의수
+    '''
+    numerator=factorial(n)
+    denominator=factorial(n-r)*factorial(r)
+
+    return numerator/denominator
+
+
+if __name__=="__main__":
+    n = int(input("Input n: "))
+    r = int(input("Input r: "))
+    print(f"{n}C{r} = {nCr(n,r)}")
+
+
