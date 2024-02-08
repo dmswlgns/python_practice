3577acd05ab16c79a906e877f1ecea2b7d9c4ad2
jihun <altmxjdms@naver.com>
2024-02-08 10:40:42 +0900

    02_08_v1.3

--- python02_08.py
+++ python02_08.py
@@ -1,14 +1,25 @@
 
-def factorial(num) -> int :
-    '''
+# def factorial(num) -> int :
+#     '''
+#     반복문을 이용한 팩토리얼 계산
+#     :param num:  num!
+#     :return: 팩토리얼 값
+#     '''
+#     result=1
+#     for i in range(1,num+1):
+#         result *=i
+#     return result
 
-    :param num:  num!
-    :return: 팩토리얼 값
+def factorial(num)->int:
     '''
-    result=1
-    for i in range(1,num+1):
-        result *=i
-    return result
+    재쉬함수를 이용함
+    :param num:
+    :return:
+    '''
+    if num==1:
+        return 1
+    else:
+        return num*factorial(num-1)
 
 def nCr(n,r) -> int :
     '''
@@ -24,8 +35,9 @@ def nCr(n,r) -> int :
 
 
 if __name__=="__main__":
-    n = int(input("Input n: "))
-    r = int(input("Input r: "))
-    print(f"{n}C{r} = {nCr(n,r)}")
-
+    # n = int(input("Input n: "))
+    # r = int(input("Input r: "))
+    # print(f"{n}C{r} = {nCr(n,r)}")
+    f=int(input())
+    print(factorial(f))
 
