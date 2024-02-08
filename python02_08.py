cb08c77e0c1e907bf9d578ea57138a0e415b20c5
jihun <altmxjdms@naver.com>
2024-02-08 13:50:39 +0900

    02_08_v1.7

--- python02_08.py
+++ python02_08.py
@@ -1,13 +1,25 @@
 import mymath
-import time
+import random
+# if __name__=="__main__":
+#     n = int(input("Input n: "))
+#     r = int(input("Input r: "))
+#     print(f"{n}C{r} = {mymath.nCr(n,r)}")
+#
 
-if __name__=="__main__":
-    n = int(input("Input n: "))
-    r = int(input("Input r: "))
-    print(f"{n}C{r} = {mymath.nCr(n,r)}")
-    end=time.time()
+answer=random.randint(1,100)
+chance=7
 
-
-    # f=int(input())
-    # print(mymath.factorial(f))
-
+while chance!=0:
+    guess_number=int(input("Input guess number:"))
+    if guess_number==answer:
+        print(f"You Win.Answer is {answer}")
+        print(f"you got it")
+        break
+    elif guess_number>answer:
+        chance=chance-1
+        print(f"guess number is lower. chance:{chance}")
+    else:
+        chance = chance - 1
+        print(f"guess number is higher. chance:{chance}")
+else:
+    print(f"Game Over. answer is {answer}")
