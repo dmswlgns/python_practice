fd80e983eb3ebaa0a4e07c4521002f3f9b7d4e91
jihun <altmxjdms@naver.com>
2024-02-08 10:26:51 +0900

    02_08_v1.2

--- python02_08.py
+++ python02_08.py
@@ -1,6 +1,14 @@
 
 def factorial(num) -> int :
-    pass
+    '''
+
+    :param num:  num!
+    :return: 팩토리얼 값
+    '''
+    result=1
+    for i in range(1,num+1):
+        result *=i
+    return result
 
 def nCr(n,r) -> int :
     '''
@@ -12,7 +20,7 @@ def nCr(n,r) -> int :
     numerator=factorial(n)
     denominator=factorial(n-r)*factorial(r)
 
-    return numerator/denominator
+    return int(numerator/denominator)
 
 
 if __name__=="__main__":
